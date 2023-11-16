247d8961a8f445a7cf442f9b58d70e667a2210ab
limye2 <144992260+limye2@users.noreply.github.com>
2023-11-16 16:08:10 +0900

    Initial Commit

---
+++ 11-5/main.c
@@ -0,0 +1,27 @@
+//
+//  main.c
+//  11-5
+//
+//  Created by MacBook Air on 2023/11/16.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    char*pc;
+    int*pi;
+    double*pd;
+    
+    pc = (char*)10000;
+    pi = (int*)10000;
+    pd = (double*)10000;
+    
+    printf("증가 전 : pc = %d, pi = %d. pd = %d\n", pc, pi, pd);
+    
+    pc++;
+    pi++;
+    pd++;
+    printf("증가 후 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+    
+    return 0;
+}
